33541f477a713389bc87b817779550b74173b2b4
joetaishi <paankrubclub@hotmail.com>
2020-09-16 21:56:59 +0700

    JS Homework

---
+++ Javascript/Homework/3/Homework3.js
@@ -0,0 +1,59 @@
+
+let loginScreen = prompt('Login');
+let message 
+
+
+if ( loginScreen == "Admin") {
+    alert( message="What is the password");
+    // return true;
+
+}
+   
+
+else if ( loginScreen == "") {
+    alert( message="Canceled");
+    // return false;
+}
+
+
+else{
+    alert( message="I dont know you!!");
+    // return false;
+
+}
+
+
+
+
+
+
+let loginScreen2 = prompt('Password');
+let message2
+
+
+if ( loginScreen2 == "codecamp#5") 
+{
+    alert( message="Welcome!!! :D");}
+
+else if ( loginScreen2 == "") 
+{
+    alert( message2="Canceled");}
+
+else
+{
+    alert( message2="Wrong Password")}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
