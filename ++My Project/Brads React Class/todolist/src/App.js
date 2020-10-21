afd090622f33f82ab3ac252cfcf5c34e1d752a8f
Fuzbu <70315100+joetaishi@users.noreply.github.com>
2020-10-21 16:26:48 +0700

    Update

--- ++My Project/Brads React Class/todolist/src/App.js
+++ ++My Project/Brads React Class/todolist/src/App.js
@@ -23,11 +23,33 @@ class App extends Component {
     ]
   }
 
+
+
+  //Toggle Complete//
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo;
+      })
+    })
+
+  }
+
+
+  //Delete ToDo//
+  delTodo = (id) => {
+    console.log(id);
+  }
+
   render() {
     console.log(this.state.todos);
     return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete}
+          delTodo={this.delTodo} />
       </div>
     );
   }
