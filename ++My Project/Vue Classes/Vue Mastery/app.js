6962d6a780b6614c76f51afe6f0b0469fed48642
Fuzbu <70315100+joetaishi@users.noreply.github.com>
2020-10-24 19:54:46 +0700

    vue mastery

---
+++ ++My Project/Vue Classes/Vue Mastery/app.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    description: 'Socks are stinky',
+    image: './picture/vmSocks-green-onWhite.jpg',
+    inventory: 0,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    cart: 0
+  },
+
+})
